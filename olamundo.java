9c039e68ec724589783c828f611ebea5c2b2df2e
EnzoAndradeF <enzoandradef.08@gmail.com>
2026-07-28 09:09:35 -0300

    fedora

--- olamundo.java
+++ olamundo.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class olamundo {
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+        System.out.println("testando");
+        Scanner scanner = new Scanner(System.in);
+        
     }
 }
